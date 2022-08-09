e2e4764b0212217a56fdc9f119611d85b0bc4cc0
Goose <Hus.O.Y@nmu.one>
2022-08-09 22:35:32 +0300

    added complex filter

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let characters = [];
+let arr = [];
 const searchInput = document.querySelector('.search_input');
 const searchBtn = document.querySelector('.search_btn');
 const selector = document.querySelector('#genderSelect');
+const selectorHouse = document.querySelector('#houseSelect');
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', reset);
 searchBtn.addEventListener('click', search);
 selector.addEventListener('change', filterByGender);
+selectorHouse.addEventListener('change', filterByHouse);
 
 axios.get('http://hp-api.herokuapp.com/api/characters')
     .then(function (response) {
         const { data } = response;
         data.splice(24, data.length);
         characters = data.map(character => new Img(character.image, character.name, character.gender, character.house));
+        arr = data.map(character => new Img(character.image, character.name, character.gender, character.house));
         renderListOfCharacters(characters);
     }).catch(err => {
         console.error(err);
@@ -30,10 +36,12 @@ function clearListOfCharacters(list) {
 function search() {
     clearListOfCharacters(characters);
     const searchText = searchInput.value.toLowerCase();
-    const filteredArray = characters.filter(character => {
+    let filteredArray = characters.filter(character => {
         return character.name.toLowerCase().includes(searchText);
     });
     renderListOfCharacters(filteredArray);
+    characters = filteredArray;
+    return characters;
 };
 
 function filterByGender() {
@@ -42,8 +50,30 @@ function filterByGender() {
     if (selectedGender == 'all') {
         renderListOfCharacters(characters);
     };
-    const filteredArray = characters.filter(character => character.gender.toLowerCase() === selectedGender);
+    let filteredArray = characters.filter(character =>{
+       return character.gender.toLowerCase() === selectedGender
+    });
     renderListOfCharacters(filteredArray);
+    characters = filteredArray;
+    return characters;
 }
 
-	
+function filterByHouse(){
+    clearListOfCharacters(characters);
+    const selectedHouse = this.value;
+    if (selectedHouse == 'all') {
+        renderListOfCharacters(characters);
+    };
+    let filteredArray = characters.filter(character =>{
+        return character.house.toLowerCase() === selectedHouse
+    } );
+    renderListOfCharacters(filteredArray);
+    characters = filteredArray;
+    return characters;
+}	
+
+function reset(){
+    clearListOfCharacters(characters);
+    renderListOfCharacters(arr);
+    searchInput.value = "";
+};
